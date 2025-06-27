252872b4ca428acbfa8d3c777dda167ba4a2f609
Harsh More <harshvmore1@gmail.com>
2025-06-27 14:12:06 +0530

    setting up backend

---
+++ backend/MentalHealth/src/test/java/com/mindnest/MentalHealth/MentalHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindnest.MentalHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
